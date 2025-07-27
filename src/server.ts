4112a763705768b2651cb7137227ca90889dfaa5
haikalrafifas <haikalrafifas@gmail.com>
2025-07-27 09:00:58 +0700

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,25 @@
+// inisialisasi
+require('dotenv').config();
+const express = require('express');
+const multer = require('multer');
+
+const apiRoutes = require('./routes');
+
+const app = express();
+const upload = multer();
+const PORT = process.env.PORT || 3000;
+
+// middleware request body, jika diperlukan
+app.use(upload.any());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// handle seluruh request /api/* ke route API
+app.use('/api', apiRoutes);
+
+// event loop
+app.listen(PORT, () => {
+  console.log(`Server berjalan di http://localhost:${PORT}`);
+});
+
+export default app;
